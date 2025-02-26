0e79d44af80b0ed58d0691dbf6e1c080e58ce6b6
Akash Antony <kshntn@gmail.com>
2025-02-26 19:14:08 +0100

    Project 1

--- Deep Learning with PyTorch/Week 6/training_pipeline/trainer/visualizer.py
+++
@@ -1,9 +0,0 @@
-# # <font style="color:blue">Visualizer Base Class</font>
-
-from abc import ABC, abstractmethod
-
-
-class Visualizer(ABC):
-    @abstractmethod
-    def update_charts(self, train_metric, train_loss, test_metric, test_loss, learning_rate, epoch):
-        pass
